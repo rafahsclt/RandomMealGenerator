7e45fc2ea1fc23f91629f84e5910a0c865810c5a
Rafael Leonen <rafah.sclt@gmail.com>
2020-09-16 21:44:26 -0300

    Finalização página principal

--- src/page/Main/index.js
+++ src/page/Main/index.js
@@ -6,19 +6,32 @@ import { FaHamburger } from 'react-icons/fa'
 
 function Main() {
   const [initialState, setInitialState] = useState(true)
-  const [recipe, setRecipe] = useState([])
+  const [recipe, setRecipe] = useState({})
+  const [ingredients, setIngredients] = useState([])
+  const [measures, setMeasures] = useState([])
 
   const getRecipe = useCallback(async () => {
     const response = await api.get('')
-    setRecipe(response.data)
+    setRecipe(response.data.meals[0])
     setInitialState(false);
-  }, [])
+
+    const ingr = []
+    const meas = []
+
+    Object.keys(response.data.meals[0]).forEach(key => {
+      if(key.startsWith('strIngredient')) ingr.push(response.data.meals[0][key])
+      if(key.startsWith('strMeasure')) meas.push(response.data.meals[0][key])
+    })
+
+    setIngredients(ingr)
+    setMeasures(meas)
+  }, [recipe, initialState])
 
   return (
     <Container >
       <Interactive initialState={initialState}>
-        <Title>Sentindo fome?</Title>
-        <SubTitle>Aprenda uma nova receita aleatória</SubTitle>
+        {initialState && <Title>Sentindo fome?</Title> }
+        <SubTitle initialState={initialState}>Aprenda uma nova receita aleatória</SubTitle>
         <GetRecipeButton
           onClick={getRecipe}
         >
@@ -26,19 +39,46 @@ function Main() {
           <FaHamburger color="#EA1D2C" size={24} />
         </GetRecipeButton>
       </Interactive>
+      {!initialState && 
       <Content>
         <MealTitleSection>
-
+          <h2>
+            Name: 
+            <span>{recipe.strMeal} ({recipe.strCategory})</span>
+          </h2>
+          <h2>
+            From:
+            <span>{recipe.strArea}</span>
+          </h2>
         </MealTitleSection>
         <MealSection>
           <IngredientsSection>
-
+            <img src={recipe.strMealThumb} />
+            <div>
+              <h1>Ingredientes</h1>
+              <ul>
+                {ingredients.map((ingred, index) => {
+                  if(ingred) {
+                    return <li><span>{ingred}</span><span>{measures[index]}</span></li>
+                  }
+                  else {
+                    return
+                  }
+                })
+                }
+              </ul>
+            </div>
           </IngredientsSection>
           <VideoSection>
-
+            <iframe src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}`} />
+            <div>
+              <h1>Como preparar</h1>
+              <p>{recipe.strInstructions}</p>
+            </div>
           </VideoSection>
         </MealSection>
       </Content>
+      }
     </Container>
   );
 }
--- src/page/Main/style.js
+++ src/page/Main/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { shade } from 'polished'
 
 export const Container = styled.div`
@@ -11,9 +11,15 @@ export const Interactive = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 20px;
+    padding: 10px;
+    height: 100%;
 
-    height: ${props => props.initialState ? '100%' : '20%'};
+    ${props => !props.initialState && css`
+        height: 10%;
+        border-bottom: 2px solid #000;
+        flex-direction: row;
+        justify-content: space-around;
+    `}
 `
 
 export const Title = styled.h1`
@@ -24,7 +30,7 @@ export const Title = styled.h1`
 
 export const SubTitle = styled.h2`
     font-size: 26px;
-    margin-bottom: 20px;
+    margin-bottom: ${props => props.initialState ? '20px' : '0' };
 `
 
 export const GetRecipeButton = styled.button`
@@ -51,21 +57,108 @@ export const GetRecipeButton = styled.button`
 `
 
 export const Content = styled.div`
-    display: ${props => props.initialState ? 'none' : 'flex'};
+    display: flex;
     background-color: #fff;
+    height: 90%;
+    flex-direction: column;
 `
 export const MealTitleSection = styled.div`
+    display: flex;
+    height: 10%;
+    width: 100%;
 
+    align-items: center;
+    justify-content: space-around;
+
+    > h2 {
+        font-size: 22px;
+        font-weight: bold;
+        color: #000;
+        
+        span {
+            margin-left: 5px;
+            font-weight: 400;
+        }
+    }
 `
 
 export const MealSection = styled.div`
-
+    display: flex;
+    flex-direction: row;
+    height: 90%;
 `
 
 export const IngredientsSection = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 0 30px;
+    border-right: 2px solid #000;
 
+    img {
+        align-self: center;
+        width: 80%;
+        height: 300px;
+        margin-bottom: 10px;
+    }
+
+    > div {
+        padding: 0 60px;
+        display: flex;
+        flex-direction: column;
+        h1 {
+            align-self: center;
+            font-size: 22px;
+            color: #000;
+            margin-top: 5px;
+            margin-bottom: 10px;
+            font-weight: bold;
+            font-style: italic;
+        }
+
+        ul {
+            color: #000;
+            font-size: 18px;
+            li {
+                display: flex;
+                justify-content: space-between;
+            }
+        }
+    }
 `
 
 export const VideoSection = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 0 40px;
 
+    iframe {
+        align-self: center;
+        width: 80%;
+        height: 300px;
+        margin-bottom: 10px;
+    }
+
+    > div {
+        padding: 0 60px;
+        display: flex;
+        flex-direction: column;
+
+        h1 {
+            align-self: center;
+            font-size: 22px;
+            color: #000;
+            margin-top: 5px;
+            margin-bottom: 10px;
+            font-weight: bold;
+            font-style: italic;
+        }
+
+        p {
+            margin-top: 10px;
+            color: #000;
+            font-size: 18px;
+        }
+    }
 `
